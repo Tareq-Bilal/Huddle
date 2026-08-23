16854d919ead034118627954ed4d60bfd1a6245d
Tareq-Bilal <tareqbilal44@gmail.com>
2026-08-23 23:42:17 +0300

    add pino and pino-http for enhanced logging

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-import userRouter from './routes/userRoutes';
-import authRouter from './routes/authRoutes';
+import pino from 'pino-http';
+import userRouter from './routes/userRoutes.ts';
+import authRouter from './routes/authRoutes.ts';
 
 const app = express();
+const pinoLogger = pino();
 
+app.use(pinoLogger);
+
+app.get('/', function (req, res) {
+  req.log.info('something')
+  res.send('hello world')
+})
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok', message: 'Server is healthy' });
